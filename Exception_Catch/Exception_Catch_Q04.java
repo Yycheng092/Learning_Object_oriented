fc310ddaf45bb16abe1420bda9a8fecaee95f614
YU-Cheng Yang <142778228+Yycheng092@users.noreply.github.com>
2025-04-21 21:33:13 +0800

    Update Exception_Catch_Q04.java

--- Exception_Catch/Exception_Catch_Q04.java
+++ Exception_Catch/Exception_Catch_Q04.java
@@ -1,1 +1,28 @@
+// 輸入模組
+import java.util.Scanner;
+
+public class Exception_Catch_Q01 {
+
+          public static void main(String[] args) {
+
+                    // 輸入連續三組數字
+                    Scanner input_number = new Scanner(System.in);
+
+                    // 將輸入的數字分成三組，一組兩個數字
+                    for (int i = 0; i < 3; i++) {
+
+                              // 將輸入是為字串讀取並依照空白分割成兩個索引陣列
+                              String line = input_number.nextLine();
+                              String[] split = line.trim().split("\\s+");
+
+                              try {
+                                        // 將 split 陣列分別轉換成整數，若過程中發生錯誤則會進入例外處理
+                                        int a = Integer.parseInt(split[0]) % Integer.parseInt(split[1]);
+                                        System.out.println(a);
+                              } catch (Exception e) {
+                                        System.out.println("input error");
+                              }
+                    }
+          }
+}
 
